dd431586f39492c1b16a19cec9c088513ab8f3b8
y_mc <momo.toure27@gmail.com>
2025-06-09 20:11:57 +0200

    update

--- src/bytecode/mod.rs
+++ src/bytecode/mod.rs
@@ -7,9 +7,9 @@ pub mod instructions;
 pub mod opcodes;
 
 // Dans bytecode/mod.rs
-pub fn calculate_branch_offset(from_addr: u32, to_addr: u32, instr_size: u32) -> i32 {
-    // Pour un saut relatif, l'offset est calculé à partir de l'adresse
-    // de l'instruction SUIVANTE (from_addr + instr_size)
-    let next_addr = from_addr + instr_size;
-    (to_addr as i64 - next_addr as i64) as i32
-}
+// pub fn calculate_branch_offset(from_addr: u32, to_addr: u32, instr_size: u32) -> i32 {
+//     // Pour un saut relatif, l'offset est calculé à partir de l'adresse
+//     // de l'instruction SUIVANTE (from_addr + instr_size)
+//     let next_addr = from_addr + instr_size;
+//     (to_addr as i64 - next_addr as i64) as i32
+// }
